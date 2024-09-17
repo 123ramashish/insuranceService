3f04fcebb82684022a4c302c62379929a7170cdc
123ramashish <rashish1349@gmail.com>
2024-09-18 00:34:02 +0530

    UI setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-rose-300">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+// import lineclamp from "@tailwindcss/line-clamp";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
